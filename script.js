c591548338a5974c021ce3784a401d6dde174d6d
Lynn-cc <babyliner1026@gmail.com>
2017-09-12 19:10:46 +0800

    加动画

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 $(function(){
-    window.pageSlide = new mo.Slide({
+    var pageSlide = new mo.Slide({
         target: $('.slide li'),
         effect: 'slide'
     });
+
+    pageSlide.on('change', function(e, index) {
+        $('li').eq(index).find('[data-ani]').each(function() {
+            var $el = $(this);
+            animate($el, $el.data('ani'));
+        });
+    });
+
+    function animate($el, name) {
+        $el.addClass('animated').addClass(name);
+        setTimeout(function() {
+            $el.removeClass(name);
+        }, 1000);
+    }
 });
